04167f40fdc2bc8f1b1c06504c82657fa8d16b19
Michael Dorst <m@mdorst.net>
2021-05-16 15:58:46 -0700

    Clean up config

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,134 @@
-use std::collections::HashMap;
 use std::process::Command;
 use std::thread;
 use umberwm::{
     umberwm, Actions, Conf, CustomAction, DisplayBorder, EventsCallbacks, Keybind, WindowBorder,
-    MOD_MASK_1, MOD_MASK_SHIFT,
+    MOD_MASK_1, MOD_MASK_CONTROL, MOD_MASK_SHIFT,
 };
 
 fn main() {
     let meta = MOD_MASK_1;
 
-    let conf = Conf {
-        meta,
-
-        display_borders: vec![
-            DisplayBorder {
-                left: 0,
-                right: 0,
-                top: 0,
-                bottom: 0,
-                gap: 4,
-            },
-            DisplayBorder {
-                left: 0,
-                right: 0,
-                top: 0,
-                bottom: 0,
-                gap: 0,
-            },
-        ],
-
-        border: WindowBorder {
-            width: 1,
-            focus_color: 0x906cff,
-            normal_color: 0x000000,
+    let display_borders = vec![
+        DisplayBorder {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            gap: 4,
         },
+        DisplayBorder {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            gap: 0,
+        },
+    ];
 
-        workspaces_names: vec![(1..9).map(|i| i.to_string()).collect()],
+    let border = WindowBorder {
+        width: 1,
+        focus_color: 0x906cff,
+        normal_color: 0x000000,
+    };
 
-        custom_actions: vec![
-            (
-                Keybind::new(meta, "space"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("rofi").arg("-show").arg("run").status();
-                    });
-                }) as CustomAction,
-            ),
-            (
-                Keybind::new(meta | MOD_MASK_SHIFT, "Return"),
-                Box::new(|| {
-                    thread::spawn(move || {
-                        let _ = Command::new("alacritty").status();
-                    });
-                }),
-            ),
-            (
-                Keybind::new(meta | MOD_MASK_SHIFT, "q"),
-                Box::new(|| std::process::exit(0)),
-            ),
-        ]
-        .into_iter()
-        .collect::<HashMap<Keybind, CustomAction>>(),
+    let workspaces_names = vec![(1..9).map(|i| i.to_string()).collect()];
 
-        wm_actions: vec![
-            (
-                Keybind::new(meta | MOD_MASK_SHIFT, "c"),
-                Actions::CloseWindow,
-            ),
-            (
-                Keybind::new(meta | MOD_MASK_SHIFT, "m"),
-                Actions::ChangeLayout,
-            ),
-            (Keybind::new(meta | MOD_MASK_SHIFT, "g"), Actions::ToggleGap),
-            (
-                Keybind::new(meta | MOD_MASK_SHIFT, "r"),
-                Actions::SerializeAndQuit,
-            ),
-        ]
-        .into_iter()
-        .collect::<HashMap<Keybind, Actions>>(),
+    let custom_actions = vec![
+        (
+            Keybind::new(meta, "space"),
+            Box::new(|| {
+                thread::spawn(move || {
+                    let _ = Command::new("rofi").arg("-show").arg("run").status();
+                });
+            }) as CustomAction,
+        ),
+        (
+            Keybind::new(meta | MOD_MASK_SHIFT, "Return"),
+            Box::new(|| {
+                thread::spawn(move || {
+                    let _ = Command::new("alacritty").status();
+                });
+            }),
+        ),
+        (
+            Keybind::new(meta | MOD_MASK_CONTROL, "q"),
+            Box::new(|| std::process::exit(0)),
+        ),
+    ]
+    .into_iter()
+    .collect();
 
-        // Won't tile windows with WM_CLASS's in this list
-        ignore_classes: vec!["xscreensaver", "Discover-overlay"]
-            .into_iter()
-            .map(|x| x.to_string())
-            .collect(),
+    let wm_actions = vec![
+        (
+            Keybind::new(meta | MOD_MASK_SHIFT, "c"),
+            Actions::CloseWindow,
+        ),
+        (
+            Keybind::new(meta | MOD_MASK_SHIFT, "m"),
+            Actions::ChangeLayout,
+        ),
+        (Keybind::new(meta | MOD_MASK_SHIFT, "g"), Actions::ToggleGap),
+        (
+            Keybind::new(meta | MOD_MASK_CONTROL, "r"),
+            Actions::SerializeAndQuit,
+        ),
+    ]
+    .into_iter()
+    .collect();
 
-        float_classes: vec![
-            "confirm",
-            "dialog",
-            "error",
-            "splash",
-            "toolbar",
-            "screenkey",
-            "audacious",
-            "Download",
-            "dropbox",
-            "file_progress",
-            "file-roller",
-            "Komodo_confirm_repl",
-            "Komodo_find2",
-            "pidgin",
-            "skype",
-            "Transmission",
-            "Update",
-            "Xephyr",
-            "obs",
-            "rofi",
-            "xscreensaver",
-            "quickmarks",
-            "discover-overlay",
-            "Discover-overlay",
-        ]
+    // Won't tile windows with WM_CLASS's in this list
+    let ignore_classes = vec!["xscreensaver", "Discover-overlay"]
         .into_iter()
         .map(|x| x.to_string())
-        .collect(),
+        .collect();
 
-        events_callbacks: EventsCallbacks {
-            on_change_workspace: None,
-        },
+    let float_classes = vec![
+        "confirm",
+        "dialog",
+        "error",
+        "splash",
+        "toolbar",
+        "screenkey",
+        "audacious",
+        "Download",
+        "dropbox",
+        "file_progress",
+        "file-roller",
+        "Komodo_confirm_repl",
+        "Komodo_find2",
+        "pidgin",
+        "skype",
+        "Transmission",
+        "Update",
+        "Xephyr",
+        "obs",
+        "rofi",
+        "xscreensaver",
+        "quickmarks",
+        "discover-overlay",
+        "Discover-overlay",
+    ]
+    .into_iter()
+    .map(|x| x.to_string())
+    .collect();
 
-        with_gap: true,
+    let events_callbacks = EventsCallbacks {
+        on_change_workspace: None,
     };
-    umberwm(conf).run();
+
+    let with_gap = true;
+
+    umberwm(Conf {
+        meta,
+        display_borders,
+        border,
+        workspaces_names,
+        custom_actions,
+        wm_actions,
+        ignore_classes,
+        float_classes,
+        events_callbacks,
+        with_gap,
+    })
+    .run();
 }
